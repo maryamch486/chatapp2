0f58ef27daa14f2aa8f8577dd0074435f4516868
maryamch486 <maryamsadiq1616@gmail.com>
2020-12-17 21:24:17 -0800

    chat screen

---
+++ lib/widget/chat_list.dart
@@ -0,0 +1,25 @@
+import 'package:chatapp/widget/chat_message.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class   ChatList extends StatelessWidget{
+  final List <Map<String, dynamic>> snapshot;
+  const ChatList({Key key, this.snapshot}) : super(key:key);
+  @override
+Widget build (BuildContext Context){
+    return Flexible(
+        child: ListView.builder(
+          padding: EdgeInsets.all(8.0),
+      reverse: true,
+      itemCount: snapshot.length,
+          itemBuilder: (_, int index)=> ChatMessage(
+            snapshot: snapshot[index],
+          ),
+        ),
+    );
+
+    return Container();
+
+  }
+
+}
--- lib/widget/chat_message.dart
+++ lib/widget/chat_message.dart
@@ -1,17 +1,21 @@
-
+import 'package:chatapp/model/record.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget{
   final Map<String, dynamic> snapshot;
-  const ChatMessage({Key key, this .snapshot }) :super(key:key);
+  const ChatMessage({Key key, this.snapshot}) : super(key:key);
   @override
   Widget build(BuildContext context) {
 
-    return Container();
-
-
-
-
+    final record= Record.fromMap(snapshot);
+    return Container(
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(child: Text(record.name[0]),),
+          Text(record.text),
+        ],
+      ),
+    );
   }
 }
 
